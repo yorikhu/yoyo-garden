f9d6f06487285ef4a2b900d5aef7ca5c72de0b0b
yorikhu <shallowink1999@163.com>
2024-04-09 22:09:39 +0800

    feat: responsive menu

---
+++ src/app/garden/about/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.scss";
+
+export default function About() {
+  return <div className={styles["about"]}></div>;
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,48 @@
+"use client";
 import Link from "next/link";
 import styles from "./index.module.scss";
+import { usePathname } from "next/navigation";
+import cls from "classnames";
+
+const navList = [
+  {
+    name: "HOME",
+    link: "/garden/home",
+  },
+  {
+    name: "BLOG",
+    link: "/garden/blog",
+  },
+  {
+    name: "About",
+    link: "/garden/about",
+  },
+];
 
 function Header() {
-  const navList = [
-    {
-      name: "HOME",
-      link: "/home",
-    },
-    {
-      name: "BLOG",
-      link: "/blog",
-    },
-    {
-      name: "About",
-      link: "/about",
-    },
-  ];
+  const pathname = usePathname();
 
   return (
     <div className={styles["header"]}>
       <div className={styles["left-contain"]}>
         <h2 className={styles["main-logo"]}>
-          <Link href="/home">Yoyo's Garden</Link>
+          <Link href="/garden/home">
+            Yoyo&lsquo;s Garden
+          </Link>
         </h2>
       </div>
       <div className={styles["right-contain"]}>
         <ul className={styles["nav-list"]}>
           {navList.map((nav) => (
-            <li className={styles["nav-item"]}>
-              <Link href={nav.link}>{nav.name}</Link>
+            <li
+              key={nav.name}
+              className={cls(styles["nav-item"], {
+                [styles["active"]]: nav.link === pathname,
+              })}
+            >
+              <Link href={nav.link}>
+                {nav.name}
+              </Link>
             </li>
           ))}
         </ul>
