c7ba87de5437ef6484ffbbb10e8024cd72842ec9
yorikhu <shallowink1999@163.com>
2024-05-05 16:51:54 +0800

    feat: performance optimization

--- src/components/Typing/index.tsx
+++ src/components/Typing/index.tsx
@@ -1,47 +1,56 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./index.module.scss";
-import { useOneceEffect } from "@/hooks/common";
 import { sleep } from "@/utils/common";
 
 type Props = {
   textList?: string[];
 };
 
-function Typing({
-  textList = ["花开堪折直需折，莫待无花空折枝", "测试文案"],
-}: Props) {
+function Typing({ textList = [] }: Props) {
   const [currentText, setCurrentText] = useState<string>("");
 
-  const typing = async (current: string, text: string) => {
-    for (let i of text) {
-      current += i;
-      setCurrentText(current);
-      await sleep(100);
+  class TextListLoop {
+    needClear: boolean = false;
+    async typing(current: string, text: string) {
+      for (let i of text) {
+        if (this.needClear) break;
+        current += i;
+        setCurrentText(current);
+        await sleep(100);
+      }
+      await sleep(1000);
+      while (current.length > 0) {
+        if (this.needClear) break;
+        current = current.slice(0, current.length - 1);
+        setCurrentText(current);
+        await sleep(50);
+      }
     }
-    await sleep(1000);
-    while (current.length > 0) {
-      current = current.slice(0, current.length - 1);
-      setCurrentText(current);
-      await sleep(50);
+    async runTextListLoop() {
+      if (this.needClear) return;
+      const text = textList.shift();
+      if (!text) return;
+      textList.push(text);
+      await this.typing(currentText, text);
+      this.runTextListLoop();
     }
-  };
+    async clearTextListLoop() {
+      this.needClear = true;
+    }
+  }
 
-  const runTextListLoop = async () => {
-    const text = textList.shift();
-    if (!text) return;
-    await typing(currentText, text);
-    textList.push(text);
-    runTextListLoop();
-  };
-
-  useOneceEffect(() => {
-    runTextListLoop();
+  useEffect(() => {
+    const loop = new TextListLoop();
+    loop.runTextListLoop();
+    return () => {
+      loop.clearTextListLoop();
+    };
   }, []);
 
   return (
     <div className={styles["typing"]}>
-      <p className={styles["text-contain"]}>/* {currentText} */</p>
+      <p className={styles["text-contain"]}>{`/* ${currentText} */`} </p>
     </div>
   );
 }
--- src/hooks/common.tsx
+++ src/hooks/common.tsx
@@ -1,14 +1,22 @@
 import { DependencyList, EffectCallback, useEffect, useRef } from "react";
 
+type Destructor = () => void;
+
+/** 受限于 destructor 的表现, 仅限于开发环境调试使用 */
 export const useOneceEffect = (
   effect: EffectCallback,
   deps?: DependencyList | undefined
 ) => {
   const executed = useRef(false);
   useEffect(() => {
-    if (!executed.current) {
-      executed.current = true;
-      effect();
+    let destructor: void | Destructor = () => {};
+
+    if (executed.current) {
+      destructor = effect();
     }
+
+    executed.current = true;
+
+    return destructor;
   }, deps);
 };
