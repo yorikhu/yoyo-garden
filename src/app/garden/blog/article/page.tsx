3952ca5b7b2684366966357ced732daa29f9f96a
yorikhu <shallowink1999@163.com>
2024-05-13 22:31:57 +0800

    feat: article page

---
+++ src/app/garden/blog/article/page.tsx
@@ -0,0 +1,20 @@
+import MarkdownView from "@/components/MarkdownView";
+import styles from "./page.module.scss";
+
+export default function Blog() {
+  const md = `
+  # 标题1
+  ## 标题2
+  ### 标题3
+1. 列表1
+2. 列表2
+\`\`\`js
+console.log("123")
+\`\`\`
+  `;
+  return (
+    <div className={styles["article"]}>
+      <MarkdownView children={md} />
+    </div>
+  );
+}
