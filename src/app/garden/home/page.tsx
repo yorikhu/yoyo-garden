30f3cded1be8cce6415f2688b80475c66d367535
yorikhu <shallowink1999@163.com>
2024-05-28 16:13:48 +0800

    feat: modify home's title

--- src/app/garden/home/page.tsx
+++ src/app/garden/home/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles['home']}>
       <div className={styles['title-group']}>
         <h2 className={styles['main-title']}>
-          <span className={styles['strong-text']}>悠悠</span>
+          <span className={styles['strong-text']}>攸悠</span>
           <span className={styles['tiny-text']}>の花园</span>
         </h2>
         <h3 className={styles['sub-title']}>Yoyo&apos;s Garden</h3>
